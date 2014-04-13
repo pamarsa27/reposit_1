317fb7b4b75b4609a27dc01f513be238d45d83e9
Familia <Familia@Familia-PC>
2014-04-13 10:48:09 +0200

    Cambio a=10 --> a=20

---
+++ src/pacogithub/PacoGitHub.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacogithub;
+
+/**
+ *
+ * @author Familia
+ */
+public class PacoGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        int a=20;
+        int b=30;
+        int auxa=a;
+        a=b;
+        b=auxa;
+        
+        System.out.println("a=" + a);
+        System.out.println("b="+b);
+    }
+}
